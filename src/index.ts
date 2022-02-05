082b638531a572b5ec1fc1e76e55c220085c8bb0
tom <tomcan1126@gmail.com>
2022-02-06 05:46:24 +0900

    勝敗判定・パターン部分の大幅修正

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,11 @@ const player2 = "x";
 let turnCount = 1;
 
 //初期表示
-const gameBoard: string[][] = [
+const GAMEBOARD: string[][] = [
   ["1", "2", "3"],
   ["4", "5", "6"],
   ["7", "8", "9"]
 ];
-
-// console.log(gameBoard);
 
 //画面表示
 function renderField(gb: string[][]): string {
@@ -29,17 +27,12 @@ function renderField(gb: string[][]): string {
   return field;
 }
 
-function main(): void {
-  init();
-  turn();
-}
-
 function init(): void {
   r.question("Enter key press start!", () => {
     // let turn = playTurn(turnCount);
 
     console.log("----");
-    console.log(renderField(gameBoard));
+    console.log(renderField(GAMEBOARD));
     console.log("----");
     console.log("先攻の人は選択する数字を入力してください");
   });
@@ -63,14 +56,14 @@ function turn(): void {
     console.log(`${nowPlayer}のターン`);
     console.log(`選択した番号${input}`);
     console.log("----");
-    console.log(renderField(gameBoard));
+    console.log(renderField(GAMEBOARD));
     console.log("----");
     judge();
   });
   // r.close();
 }
 
-//引数valの値によってgameBoardの値を書き換える
+//引数valの値によってGAMEBOARDの値を書き換える
 //書き換えるターンの値が必要
 function input_convert(val: string): void {
   const turn = playTurn(turnCount);
@@ -78,31 +71,31 @@ function input_convert(val: string): void {
 
   switch (val) {
     case "1":
-      gameBoard[0][0] = `${turn}`;
+      GAMEBOARD[0][0] = `${turn}`;
       break;
     case "2":
-      gameBoard[0][1] = `${turn}`;
+      GAMEBOARD[0][1] = `${turn}`;
       break;
     case "3":
-      gameBoard[0][2] = `${turn}`;
+      GAMEBOARD[0][2] = `${turn}`;
       break;
     case "4":
-      gameBoard[1][0] = `${turn}`;
+      GAMEBOARD[1][0] = `${turn}`;
       break;
     case "5":
-      gameBoard[1][1] = `${turn}`;
+      GAMEBOARD[1][1] = `${turn}`;
       break;
     case "6":
-      gameBoard[1][2] = `${turn}`;
+      GAMEBOARD[1][2] = `${turn}`;
       break;
     case "7":
-      gameBoard[2][0] = `${turn}`;
+      GAMEBOARD[2][0] = `${turn}`;
       break;
     case "8":
-      gameBoard[2][1] = `${turn}`;
+      GAMEBOARD[2][1] = `${turn}`;
       break;
     case "9":
-      gameBoard[2][2] = `${turn}`;
+      GAMEBOARD[2][2] = `${turn}`;
       break;
     default:
       return console.error("エラー");
@@ -110,12 +103,32 @@ function input_convert(val: string): void {
   }
 }
 
+const winPattern: number[][] = [
+  [[0][0], [0][1], [0][2]],
+  [[1][0], [1][1], [1][2]]
+];
+
+function patternCheck() {
+  // for (let i = 0; i <= winPattern.length; i++) {
+  //   let isMatch;
+  //   isMatch = String(winPattern[i]) === String(GAMEBOARD[0]) ? true : false;
+  //   console.log(isMatch);
+  //   return isMatch;
+  // }
+  // console.log(GAMEBOARD[0][0]);
+}
+
 function judge() {
   turnCount++;
   const nextplayer = playTurn(turnCount);
+  patternCheck();
   console.log(`${nextplayer}のターン`);
 }
 
+function main(): void {
+  init();
+  turn();
+}
 
 //main関数
 main();
--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,47 @@
-const gameBoard: string[][] = [['o','o','o'],['4','5','6'],['7','8','9']];
+const GAMEBOARD: string[][] = [
+  ["o", "2", "3"],
+  ["4", "o", "6"],
+  ["7", "8", "o"]
+];
 
- console.log(gameBoard[0][0] && gameBoard[0][1] && gameBoard[0][2] === 'o');
+const winPattern: number[][] = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7]
+];
 
-console.log(gameBoard);
+function patternCheck(mark: string) {
+  // console.log(GAMEBOARD);
+  // console.log(winPattern[0][1]);
+  // console.log(mark);
+  // console.log(String(GAMEBOARD));
 
-[['1','2','3'],['4','5','6'],['7','8','9']]
+  console.log("判定スタート");
+
+  const strGAME = String(GAMEBOARD).replaceAll(",", "");
+
+  let trueCount = 0;
+
+
+  winPattern.forEach((element) => {
+    console.log(element);
+    for (let i = 0; i <= strGAME.length; i++) {
+      console.log(`strGAME${i + 1}`, strGAME[i], element[0], element[1], element[2]);
+      console.log(strGAME[element[0] - 1]);
+      console.log(strGAME[element[1] - 1]);
+      console.log(strGAME[element[2] - 1]);
+    }
+  })
+  console.log("判定終了");
+}
+
+const m = "o";
+patternCheck(m);
+
+//winpatternの要素を順番に調べる
+// gameboard の
